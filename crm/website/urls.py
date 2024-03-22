57959339536ed4d341941674102c84a0e348a7fe
Siva Prakash <prakashdeveloper@outlook.in>
2024-03-22 17:25:41 +0530

    Added Register & Login pages

--- crm/website/urls.py
+++ crm/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, register_user, logout_user
 
 urlpatterns = [
     path("", home, name="home"),
+        path('logout/', logout_user, name='logout'),
+    path('register/', register_user, name='register'),
 ]
