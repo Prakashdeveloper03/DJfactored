0705ac51bc6d9cdc8edc966ad501a5dc5d1a29c0
Siva Prakash <prakashdeveloper@outlook.in>
2024-03-26 13:11:05 +0530

    Added models and migrations

--- blogapp/blogapp/urls.py
+++ blogapp/blogapp/urls.py
@@ -15,9 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import sitemaps
+from django.contrib.sitemaps.views import sitemap
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .sitemaps import CategorySitemap, PostSitemap
+
+from core.views import frontpage, about, robots_txt
+
+sitemaps = {"category": CategorySitemap, "post": PostSitemap}
 
 urlpatterns = [
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
     path("admin/", admin.site.urls),
-]
+    path("about/", about, name="about"),
+    path("", include("blog.urls")),
+    path("", frontpage, name="frontpage"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
